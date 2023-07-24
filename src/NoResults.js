c76fb297ce5e0c15a4478ddaf4a6a78705a99113
aleyna-cintron <aleynatcintron@gmail.com>
2023-07-24 18:49:44 -0500

    updated

--- src/NoResults.js
+++ src/NoResults.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BsSpotify } from 'react-icons/bs'
 
 function NoResults() {
--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BsSpotify } from 'react-icons/bs'
 import { FaSearch } from 'react-icons/fa'
 
